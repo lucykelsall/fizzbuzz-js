8f7baf11f189ddb3cd8b05f120f283474c5894ba
Lucy Kelsall <lucy.kelsall@softwire.com>
2022-10-12 14:23:50 +0100

    Printing Fizz, Buzz and FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,22 @@
-
-
 // Here, we create our main function.
 function fizzbuzz() {
-    
-    console.log('Welcome to FizzBuzz!');
+    console.log("Welcome to FizzBuzz!");
 
     // Put your code here...
-    
-    
-    // Printing numbers 1 to 100
     for (let x = 1; x < 101; x++) {
-        console.log(x)
+        if (x % 15 == 0) {
+            console.log("FizzBuzz");
+            //continue;
+        } else if (x % 3 == 0) {
+            console.log("Fizz");
+            //    continue
+        } else if (x % 5 == 0) {
+            console.log("Buzz");
+            //    continue
+        } else {
+            console.log(x);
+        }
     }
-
-
 }
 
 // Now we run the main function...
